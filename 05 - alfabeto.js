2ae09ca68cf2b73255655d7b13350458a5d0e1b2
Felipe Borges <lipex360@gmail.com>
2021-12-28 23:12:03 -0300

    ranges

--- 05 - alfabeto.js
+++ 05 - alfabeto.js
@@ -10,19 +10,50 @@ let regEx = null
 // [0-9]  -> Range
 
 regEx = /[abc]+/g
-console.log(alfabeto.match(regEx))
+console.log('01:', alfabeto.match(regEx), '\n')
 
 regEx = /[123]+/g
-console.log(alfabeto.match(regEx))
+console.log('02:', alfabeto.match(regEx), '\n')
 
 regEx = /[abc123]+/g
-console.log(alfabeto.match(regEx))
+console.log('03:', alfabeto.match(regEx), '\n')
 
 regEx = /[^abc123]/g
-console.log(alfabeto.match(regEx))
-
-regEx = /[0-9]/g
-console.log(alfabeto.match(regEx))
+console.log('04:', alfabeto.match(regEx), '\n')
 
 regEx = /[0-9]+/g
-console.log(alfabeto.match(regEx))
+console.log('05:', alfabeto.match(regEx), '\n')
+
+regEx = /[a-z]+/g
+console.log('06:', alfabeto.match(regEx), '\n')
+
+regEx = /[a-zA-Z0-9]+/g
+console.log('🏹 07:', alfabeto.match(regEx), '\n')
+
+regEx = /\w+/g
+console.log('🎯 07:', alfabeto.match(regEx), '\n')
+
+regEx = /[^a-zA-Z0-9]+/g
+console.log('🏹 08:', alfabeto.match(regEx), '\n')
+
+regEx = /\W+/g
+console.log('🎯 08:', alfabeto.match(regEx), '\n')
+
+regEx = /[0-9]+/g
+console.log('🏹 09:', alfabeto.match(regEx), '\n')
+
+regEx = /\d+/g
+console.log('🎯 09:', alfabeto.match(regEx), '\n')
+
+regEx = /[\u00a0-\u00BA]+/g
+console.log('10:', alfabeto.match(regEx), '\n')
+// https://en.wikipedia.org/wiki/List_of_Unicode_characters
+
+regEx = /(aten(c|ç)?(a|ã)?o)+/g
+console.log('11:', alfabeto.match(regEx), '\n')
+
+regEx = /\s+/g
+console.log('🎯 12:', alfabeto.match(regEx), '\n')
+
+regEx = /\S+/g
+console.log('🎯 13:', alfabeto.match(regEx), '\n')
--- base.js
+++ base.js
@@ -55,7 +55,7 @@ Os Ips são:
 `;
 
 const alfabeto =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ®¡ 0123456789 ¡';
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ®¡ 0123456789 ¡ atenção';
           
 const html = '<p>Olá mundo</p> <p>Olá de novo</p> <div>Sou a div</div>';
 const html2 = `<p 
